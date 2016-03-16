0c915c7839b49a5dd50a1e5b823a67d58bff7e31
elinnilsson <elinhejnilsson@gmail.com>
2016-03-16 13:26:21 +0100

    Added ugly hacks :D

---
+++ script.js
@@ -0,0 +1,30 @@
+(function(){
+	 
+	window.quotes = [];
+
+	var loadMatzQuotes = function(){
+		window.quotes = [
+			'I duschen kan ingen se dig gråta.',
+			'Skynda, skynda'
+			];	
+	};
+	 
+	var showMatzQuotes = function(){
+		if(window.quotes.length === 0){
+			loadMatzQuotes();
+		}
+
+		var matzQuotes = document.getElementById('matzQuotes');
+
+		matzQuotes.innerHTML = window.quotes.pop();
+	};
+
+	myAudio = new Audio('grodorna.mp3'); 
+	myAudio.addEventListener('ended', function() {
+	    this.currentTime = 0;
+	    this.play();
+	}, false);
+	myAudio.play();
+
+	document.getElementById('button').addEventListener('click', showMatzQuotes);
+})();
